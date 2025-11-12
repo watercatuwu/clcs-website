8c57f42d547e32c0a87d7917e617a02bc80ae7f1
wolf-yuan <wolf@wolf-yuan.dev>
2025-11-12 23:08:18 +0800

    feat: add logo :D

---
+++ prettier.config.js
@@ -0,0 +1,28 @@
+export default {
+  printWidth: 70,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  arrowParens: "always",
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: "never",
+  plugins: [
+    "prettier-plugin-organize-imports",
+    "prettier-plugin-astro",
+    "prettier-plugin-tailwindcss",
+    "prettier-plugin-astro-organize-imports",
+  ],
+  htmlWhitespaceSensitivity: "strict",
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
